8c9c0ec2d41f97f126fa5a1e3409df79b2b43096
Vladimir Berezovsky <berezovsky@ex.ua>
2020-03-29 18:44:07 +0300

    add sass/scss support

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  entry: './assets/scss/index.scss',
+  output: {
+    path: `${__dirname}/assets/css`
+  },
+  mode: isDevelopment ? 'development' : 'production',
+  module: {
+    rules: [
+      {
+        test: /\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: isDevelopment
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /\.(s(a|c)ss)$/,
+        loader: [
+          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: isDevelopment
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  resolve: {
+    extensions: ['.scss']
+  }
+};
